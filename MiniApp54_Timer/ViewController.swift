4637d5f2bc2339a9de6a019c4d89c29e74f22b2b
Kota <maedakoutaa@gmail.com>
2022-06-11 18:48:46 +0900

    カウンターの完成

--- MiniApp54_Timer/ViewController.swift
+++ MiniApp54_Timer/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import UICircularProgressRing
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerRing: UICircularTimerRing!
+    @IBOutlet weak var button: UIButton!
+    enum Flug {
+        case start, stop, restart
+    }
+    var flag = Flug.start
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setting()
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        switch flag {
+        case Flug.start:
+            self.flag = Flug.stop
+            self.button.setTitle("ストップ", for: .normal)
+            timerRing.startTimer(to: 360) { state in
+                switch state {
+                case .finished:
+                    self.flag = Flug.start
+                    self.button.setTitle("開始", for: .normal)
+                    //リセット
+                    self.timerRing.resetTimer()
+                case .continued:
+                    self.flag = Flug.stop
+                    self.button.setTitle("ストップ", for: .normal)
+                case .paused:
+                    self.flag = Flug.restart
+                    self.button.setTitle("再開", for: .normal)
+                }
+            }
+        case Flug.stop:
+            //ストップ
+            timerRing.pauseTimer()
+        case Flug.restart:
+            //再開
+            timerRing.continueTimer()
+        }
+    }
+
+    func setting()  {
+        timerRing.startAngle = 270
+        timerRing.isReverse = true
+        timerRing.shouldShowValueText = true
+        timerRing.outerRingColor = .clear
+        timerRing.innerRingColor = .blue
+    }
 
 }
 
